dc2b0cce6c5f7670190107e2d97cabdabc7036da
manisha <manisha.lipare17@gmail.com>
2022-12-21 17:42:32 -0500

    first commit

---
+++ src/test/java/demo1/regression1.java
@@ -0,0 +1,11 @@
+package demo1;
+
+import org.testng.annotations.Test;
+
+public class regression1 {
+	@Test
+	public void regression()
+	{
+		System.out.println("Regression");
+	}
+}
---
+++ src/test/java/demo1/smoke1.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import org.testng.annotations.Test;
+
+public class smoke1 {
+
+	@Test
+	public void smoke()
+	{
+		System.out.println("Smoke");
+	}
+	
+	
+}
